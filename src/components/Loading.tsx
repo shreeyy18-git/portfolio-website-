be10da2ecfaec5f771111ee84756e9931dcbf89e
learnedge-team <shreeyanshgithub18@gmail.com>
2026-05-28 00:07:03 +0530

    update loading screen: SA logo, Shreeyansh Asati portfolio marquee

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -46,7 +46,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          Logo
+          SA
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
@@ -62,8 +62,10 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
+            <span> Shreeyansh Asati portfolio</span>
+            <span> Shreeyansh Asati portfolio</span>
+            <span> Shreeyansh Asati portfolio</span>
+            <span> Shreeyansh Asati portfolio</span>
           </Marquee>
         </div>
         <div
